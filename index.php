5e83b7383609d7905425a655a5954541830c36fc
FIGUR <zenekmerc24@gmail.com>
2022-03-22 12:48:05 +0100

    wyswietl lekarzy

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$db = new mysqli("localhost", "root", "", "med");
+
+$q = $db->prepare("SELECT * FROM staff");
+
+if ($q->execute()){
+    $result = $q->get_result();
+    while($row = $result->fetch_assoc()){
+
+    $firstName = $row['firstName'];
+    $lastName = $row['lastName'];
+    echo "Lekarz $firstName $lastName<br>";
+    }
+} else {
+    echo "Błąd podczas wyszukiwania w bazie danych";
+}
+
+
+
+?>
